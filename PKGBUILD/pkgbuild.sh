23760d7c4aa60522c2f711049543e486f7708678
Filipe Alves <filipe.alves@bithium.com>
2023-05-30 11:15:59 +0100

    Support creating releases from other repositories
    This patch changes the `PKGBUILD/pkgbuild.sh` script so that we can
    using to generate a PKGBUILD where the remote repository used to to
    build the deb matches the current git origin url.
    
    NOTE: Currently this only works for urls present in `github`.

--- PKGBUILD/pkgbuild.sh
+++ PKGBUILD/pkgbuild.sh
@@ -46,17 +46,35 @@ if (( "${failed_checks}" )); then
 fi
 
 # Get needed variables.
-new_pkgver="$(cat .data.json | jq -r '.current_pkgver')"
+new_pkgver="$(jq -r '.current_pkgver' .data.json)"
+repo_short=$(git config --get remote.origin.url | sed -e 's|^.\+github\.com[/:]||' -e 's|\.git$||' )
+repo_user=${repo_short%%/makedeb}
+repo="https://github.com/${repo_short}.git"
+releases="$(curl -s -S "https://api.github.com/repos/${repo_short}/releases?per_page=100" | jq -r '.[].name')"
+if [[ -z "${releases}" ]]; then
+	releases="$(curl -s -S "https://api.github.com/repos/${repo_short}/tags?per_page=100" | jq -r '.[].name')"
+fi
 
 case "${RELEASE}" in
-	stable)     pkgname='makedeb' ;;
-	beta|alpha) pkgname="makedeb-${RELEASE}" ;;
-	*)          error "Invalid release '${RELEASE}'."; exit 1 ;;
+	stable)
+		pkgname='makedeb'
+		release="$(echo "${releases}" | grep 'v[0-9.-]*$')"
+		;;
+	beta|alpha|"${repo_user}")
+		pkgname="makedeb-${RELEASE}"
+		release="$(echo "${releases}" | grep "v[0-9.]*-${RELEASE}[0-9]*\$")"
+		;;
+	*)
+		error "Invalid release '${RELEASE}'.";
+		exit 1
+		;;
 esac
 
 # Get the pkgver by checking the current latest pkgver for this release, and bumping it as necesary.
+set +eo pipefail
 current_pkgver="$(apt list "${pkgname}" 2> /dev/null | grep "^${pkgname}/" | awk '{print $2}' | grep -o '^[^-]*')"
 current_pkgrel="$(apt list "${pkgname}" 2> /dev/null | grep "^${pkgname}/" | awk '{print $2}' | grep -o '[^-]*$')"
+set -eo pipefail
 
 if [[ "${new_pkgver}" != "${current_pkgver}" ]]; then
 	new_pkgrel="${RELEASE}"
@@ -67,12 +85,13 @@ else
 fi
 
 # Print out the generated PKGBUILD.
-cat PKGBUILD/TEMPLATE.PKGBUILD | sed \
-	-e "s|{{ release }}|${RELEASE}|g" \
+sed -e "s|{{ release }}|${RELEASE}|g" \
 	-e "s|{{ target }}|${TARGET}|g" \
 	-e "s|{{ pkgname }}|${pkgname}|g" \
 	-e "s|{{ pkgver }}|${new_pkgver}|g" \
-	-e "s|{{ pkgrel }}|${new_pkgrel}|g" | {
+	-e "s|{{ pkgrel }}|${new_pkgrel}|g" \
+	-e "s|^url=.\+|url=\'${repo}\'|" \
+	PKGBUILD/TEMPLATE.PKGBUILD | {
 		if [[ "${RELEASE}" == 'stable' ]]; then
 			grep -Ev '^conflicts=|^provides='
 		else
